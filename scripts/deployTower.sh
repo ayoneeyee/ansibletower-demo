f0fa9faea1fb4da0841ab232a09ecd0ef099bbb2
Magnus Glantz <sudo@redhat.com>
2017-02-27 16:53:16 +0200

    Debug

--- scripts/deployTower.sh
+++ scripts/deployTower.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 echo $(date) " - Starting Script"
 
+USER=$1
+PASSWORD="$2"
+POOL_ID=$3
+
+# Register Host with Cloud Access Subscription
+echo $(date) " - Register host with Cloud Access Subscription"
+
+subscription-manager register --username="$USER" --password="$PASSWORD"
+subscription-manager attach --pool=$POOL_ID
+
+# Disable all repositories and enable only the required ones
+echo $(date) " - Disabling all repositories and enabling only the required repos"
+
+subscription-manager repos --disable="*"
+
+subscription-manager repos \
+    --enable="rhel-7-server-rpms" \
+    --enable="rhel-7-optional-rpms" \
+    --enable="rhel-7-extras-rpms" \
+    --enable="rhel-7-supplimental-rpms"
+
 wget https://github.com/mglantz/ansibletower-demo/raw/master/ansible-tower-setup-latest.tar.gz
 gunzip ansible-tower-setup-latest.tar.gz
 tar xvf ansible-tower-setup-latest.tar
