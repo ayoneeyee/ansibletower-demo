4aba9afd6c7e745e4b53a9211a23a8266eda9148
Magnus Glantz <sudo@redhat.com>
2017-04-20 23:12:28 +0200

    Works when run manually..

--- scripts/deployTower.sh
+++ scripts/deployTower.sh
@@ -1,11 +1,14 @@
-#!/bin/bash
 echo $(date) " - Starting Script"
 
-#wget https://github.com/mglantz/ansibletower-demo/raw/master/ansible-tower-setup-latest.tar.gz
-#gunzip ansible-tower-setup-latest.tar.gz
-#tar xvf ansible-tower-setup-latest.tar
-#cd ansible-tower-setup-3.0.3
-#sh ./setup.sh
+wget https://github.com/mglantz/ansibletower-demo/raw/master/ansible-tower-setup-latest.tar.gz
+gunzip ansible-tower-setup-latest.tar.gz
+tar xvf ansible-tower-setup-latest.tar
+cd ansible-tower-setup-3.0.3
+wget https://raw.githubusercontent.com/mglantz/ansibletower-demo/master/scripts/inventory -O inventory.new
+cp inventory inventory.org
+mv inventory.new inventory
+sh ./setup.sh
+
 
 
 
